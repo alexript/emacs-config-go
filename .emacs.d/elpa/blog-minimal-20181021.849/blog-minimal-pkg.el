cabd45e251844f014378f086f9493aec2d330150
Alexander Malyshev <alexript@outlook.com>
2018-12-04 22:43:20 +0300

    update packages

---
+++ .emacs.d/elpa/blog-minimal-20181021.849/blog-minimal-pkg.el
@@ -0,0 +1,16 @@
+(define-package "blog-minimal" "20181021.849" "a simple static site generator based on org mode"
+  '((ht "1.5")
+    (simple-httpd "1.4.6")
+    (mustache "0.22")
+    (s "1.11.0")
+    (org "9.0.3"))
+  :keywords
+  '("tools")
+  :authors
+  '(("Thank Fly" . "thiefuniverses@gmail.com"))
+  :maintainer
+  '("Thank Fly" . "thiefuniverses@gmail.com")
+  :url "https://github.com/thiefuniverse/blog-minimal")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
