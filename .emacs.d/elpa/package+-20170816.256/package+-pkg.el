9c09f12e74593f83766b547c4546535322db8805
Alex 'Ript' Malyshev <alexript@gmail.com>
2019-07-05 10:28:00 +0300

    update emacs packages

--- .emacs.d/elpa/package+-20170816.256/package+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "package+" "20170816.256" "Extensions for the package library." 'nil :commit "9213f6134eabc2cff5826779ced437714324c066" :keywords '("extensions" "tools") :authors '(("Ryan Davis" . "ryand-ruby@zenspider.com")) :maintainer '("Ryan Davis" . "ryand-ruby@zenspider.com") :url "TBA")
---
+++ .emacs.d/elpa/package+-20190702.253/package+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "package+" "20190702.253" "Extensions for the package library." '((emacs "24.3")) :commit "2a99b8e27be1702d94ce077ecd75e1089fa18a32" :keywords '("extensions" "tools") :authors '(("Ryan Davis" . "ryand-ruby@zenspider.com")) :maintainer '("Ryan Davis" . "ryand-ruby@zenspider.com") :url "https://github.com/zenspider/package")
