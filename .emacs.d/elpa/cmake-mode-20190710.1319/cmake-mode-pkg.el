53493c2b8b5268ad5d14459caf8ebd4bce41729f
Alex 'Ript' Malyshev <alexript@gmail.com>
2019-08-13 14:03:12 +0300

    update emacs submodules

---
+++ .emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "7d194f7d8331a127e0b2b921dc6bc0abfe21a0f5")
