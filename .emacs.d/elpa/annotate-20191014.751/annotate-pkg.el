b7eee65f6cfb2fe279ec82a97bb579a82cedd1f6
Alex 'Ript' Malyshev <alexript@gmail.com>
2019-10-15 16:19:55 +0300

    update emacs modules

---
+++ .emacs.d/elpa/annotate-20191014.751/annotate-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "annotate" "20191014.751" "annotate files without changing them" 'nil :commit "d60feb9cabace219c5247ccbce94f56474b62ec3" :authors '(("Bastian Bechtold")) :maintainer '("Bastian Bechtold") :url "https://github.com/bastibe/annotate.el")
